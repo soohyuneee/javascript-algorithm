3ca3aa0bf2528409dca6a70d58267d8b04bf0325
soohyunee <tlstngus95@naver.com>
2023-06-01 23:36:34 +0900

    [unrated] Title: 간단한 식 계산하기, Time: 0.04 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181865. 간단한 식 계산하기/간단한 식 계산하기.js
@@ -0,0 +1,10 @@
+function solution(binomial) {
+    let arr = binomial.split(' ')
+    if (arr[1] === "+") {
+        return +arr[0] + +arr[2]
+    } else if (arr[1] === "-") {
+        return +arr[0] - +arr[2]
+    } else {
+        return +arr[0] * +arr[2]
+    }
+}
