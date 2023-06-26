b890caa5c65d5e8aef18ee2522a53dd20cf84d8b
soohyuneee <tlstngus95@naver.com>
2023-06-26 21:01:53 +0900

    [unrated] Title: 배열 만들기 2, Time: 12.38 ms, Memory: 38.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
+++ 프로그래머스/unrated/181921. 배열 만들기 2/배열 만들기 2.js
@@ -1,10 +1,11 @@
 function solution(l, r) {
-    let num = [1,2,3,4,6,7,8,9]
     let answer = [];
+    
     for (let i = l; i <= r; i++) {
         if (i.toString().replace(/['0','5']/gi, '').length === 0) {
             answer.push(i)
         }
     }
+    
     return answer.length ? answer : [-1];
 }
