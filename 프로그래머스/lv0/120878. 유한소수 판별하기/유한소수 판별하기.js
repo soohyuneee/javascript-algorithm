f20c26d270d03a957214454c9c8c04925d3eb0d3
soohyuneee <tlstngus95@naver.com>
2023-07-05 22:01:49 +0900

    [level 0] Title: 유한소수 판별하기, Time: 0.18 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
+++ 프로그래머스/lv0/120878. 유한소수 판별하기/유한소수 판별하기.js
@@ -1,3 +1,10 @@
 function solution(a, b) {
-    return Number((a / b).toFixed(10)) == a / b ? 1 : 2
+    let n = 1;
+    for (let i = 1; i <= b; i++) {
+        if (a % i === 0 && b % i === 0) n = i;
+    }
+    b = b / n;
+    while (b % 2 === 0) b = b / 2; 
+    while (b % 5 === 0) b = b / 5; 
+    return b === 1 ? 1 : 2;
 }
