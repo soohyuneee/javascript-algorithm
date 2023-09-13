a0ae55b00b8ceef05c2043f5494df08058ea1a4b
soohyuneee <tlstngus95@naver.com>
2023-09-13 22:58:58 +0900

    [level 1] Title: 삼총사, Time: 0.13 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv1/131705. 삼총사/삼총사.js
+++ 프로그래머스/lv1/131705. 삼총사/삼총사.js
@@ -1,12 +1,12 @@
 function solution(number) {
-    let arr = []
-      for(let i=0; i<number.length; i++){
-        for(let j=i+1; j<number.length; j++){
-          for (let k=j+1; k<number.length; k++){
-            arr.push(number[i]+number[j]+number[k])
-          }
+    let answer = 0;
+    for(let i =0; i < number.length-2; i++){
+        for(let j = i+1; j < number.length-1; j++){
+            for(let k = j+1; k < number.length; k++){
+                  const sum = number[i] + number[j] + number[k]
+                if(sum === 0) answer++
+            }
         }
-      }
-      let result = arr.filter((v) => v === 0).length
-      return result
+    }
+    return answer;
 }
