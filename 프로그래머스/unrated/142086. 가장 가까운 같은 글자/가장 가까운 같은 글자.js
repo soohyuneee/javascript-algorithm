9f89959850043f8e663840fb70e113558a7da3f4
soohyuneee <tlstngus95@naver.com>
2023-09-19 21:15:54 +0900

    [unrated] Title: 가장 가까운 같은 글자, Time: 2.38 ms, Memory: 36.8 MB -BaekjoonHub

--- 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
+++ 프로그래머스/unrated/142086. 가장 가까운 같은 글자/가장 가까운 같은 글자.js
@@ -1,11 +1,6 @@
 function solution(s) {
-    let answer = [];
-    
-    for(let i =0 ; i< s.length ; i++){
-        const before = s.slice(0,i)
-        answer.push(before.lastIndexOf(s[i]) === -1 ? -1 : i-before.lastIndexOf(s[i]))
-    }
-    
-    return answer;
-
+    return [...s].map((char, i) => {
+        const count = s.slice(0, i).lastIndexOf(char);
+        return count < 0 ? count : i - count;
+    });
 }
