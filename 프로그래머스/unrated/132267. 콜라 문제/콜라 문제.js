8a41ffce5c72ea0ae269c5ecbac8fe861220f957
soohyuneee <tlstngus95@naver.com>
2023-09-21 21:06:07 +0900

    [unrated] Title: 콜라 문제, Time: 0.03 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
+++ 프로그래머스/unrated/132267. 콜라 문제/콜라 문제.js
@@ -1,13 +1,10 @@
 function solution(a, b, n) {
     let answer = 0;
-
-    while (true) {
-        if (a > n) {
-            break;
-        }
-        answer += parseInt(n/a) * b;
-        n = parseInt(n/a)*b + n%a
+    
+    while (n >= a) {
+        answer += parseInt(n / a) * b;
+        n = parseInt(n / a) * b + n % a;
     }
-
+    
     return answer;
 }
