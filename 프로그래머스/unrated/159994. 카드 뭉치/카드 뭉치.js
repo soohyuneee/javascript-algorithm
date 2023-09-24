760e0fbcfc77e73b0772b73d85afb425a985f80e
soohyuneee <tlstngus95@naver.com>
2023-09-24 21:08:06 +0900

    [unrated] Title: 카드 뭉치, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
+++ 프로그래머스/unrated/159994. 카드 뭉치/카드 뭉치.js
@@ -1,19 +1,15 @@
 function solution(cards1, cards2, goal) {
-  let count = 0;
 
-  for(let i = 0 ; i < goal.length ; i += 1){
-    const findWord = goal[i];
-    
-    if (cards1[0] === findWord) {
-      cards1.shift();
-      count++;
-    }else if(cards2[0] === findWord){
-      cards2.shift();
-      count++;
-    }else {
-      return "No"
+    for(const s of goal) {
+
+        if(cards1[0] == s) {
+            cards1.shift();
+        } else if(cards2[0] == s) {
+            cards2.shift();
+        } else {
+            return "No"
+        }
     }
-  }
 
-  return goal.length === count ? "Yes" : "No"
+    return "Yes";
 }
