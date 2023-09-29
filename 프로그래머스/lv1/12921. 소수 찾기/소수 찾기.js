2b94446dc72562b466685206cdc7963edcb61f05
soohyuneee <tlstngus95@naver.com>
2023-09-29 17:40:58 +0900

    [level 1] Title: 소수 찾기, Time: 162.80 ms, Memory: 61.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.js
@@ -1,20 +1,16 @@
 function solution(n) {
-    let answer = 0;
-    const arr = new Array(n+1).fill(true); 
-    const end = Math.sqrt(n) 
-    
-    for(let i = 2; i <= end; ++i){
-        if(arr[i] === false){
-            continue; 
-        }
-        for(let k = i * i; k <= n; k += i){
-            arr[k] = false;
+    const s = new Set();
+    for(let i=1; i<=n; i+=2){
+        s.add(i);
+    }
+    s.delete(1);
+    s.add(2);
+    for(let j=3; j<Math.sqrt(n); j++){
+        if(s.has(j)){
+             for(let k=j*2; k<=n; k+=j){    
+                s.delete(k);
+             }
         }
     }
-    for(let i = 2; i <= n; ++i){
-        if(arr[i] === true){
-            answer++;
-        }
-    }
-    return answer;
+    return s.size;
 }
